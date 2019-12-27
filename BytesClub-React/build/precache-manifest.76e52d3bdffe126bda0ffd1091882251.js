938e05ec2455b0ab7e44257bddf4da09ca754eda
jayakumar.duraisamy <jayakumar.duraisamy@syncfusion.com>
2019-12-27 16:21:43 +0530

    Addded the build folder

---
+++ BytesClub-React/build/precache-manifest.76e52d3bdffe126bda0ffd1091882251.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87d33c309ac6b0b5d97b5ddcce3746d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a86b39c79f4c1df201cb",
+    "url": "/static/css/2.1f2b52a6.chunk.css"
+  },
+  {
+    "revision": "6c1e685d20afd3a21b39",
+    "url": "/static/css/main.5f568b04.chunk.css"
+  },
+  {
+    "revision": "a86b39c79f4c1df201cb",
+    "url": "/static/js/2.b57ce10c.chunk.js"
+  },
+  {
+    "revision": "6c1e685d20afd3a21b39",
+    "url": "/static/js/main.0c2180ba.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f7090debe009a19f2feaf170ccd68e1a",
+    "url": "/static/media/event1.f7090deb.png"
+  },
+  {
+    "revision": "01717c2182aa0b763b271611694ac23d",
+    "url": "/static/media/event2.01717c21.png"
+  },
+  {
+    "revision": "6a0e8d87005708f2ff4d396b816a05ac",
+    "url": "/static/media/event3.6a0e8d87.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  }
+]);
